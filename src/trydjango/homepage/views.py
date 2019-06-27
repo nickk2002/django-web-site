187d08f243f18fcf98c0879724d4dfd0c4700fd4
nickk2002 <nicolae.filat@yahoo.com>
2019-06-27 16:00:19 +0300

    another commint

--- src/trydjango/homepage/views.py
+++ src/trydjango/homepage/views.py
@@ -5,8 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return render(request, "index.html", {})
+    return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<b>Ce faci mihai?</b>");
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
--- src/trydjango/trydjango/urls.py
+++ src/trydjango/trydjango/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from homepage.views import home_view, contact_view
+from homepage.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('home/', home_view),
     path('contact/', contact_view),
+    path('about/', about_view),
 
 ]
